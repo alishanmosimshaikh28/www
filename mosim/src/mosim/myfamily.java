6676b366c7a78619f1c8985dd4bbc68c05ec8477
alishanmosimshaikh28 <121955910+alishanmosimshaikh28@users.noreply.github.com>
2023-01-14 21:20:16 +0530

    checkout
    ggggggg

---
+++ mosim/src/mosim/myfamily.java
@@ -0,0 +1,11 @@
+package mosim;
+
+public class myfamily {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("i am the member of family");
+	}
+
+}
